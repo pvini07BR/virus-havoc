56afc784c4b51042442b7e9f901ec2eea2f041fc
pvini07BR <pvini.almeida.lima@gmail.com>
2021-03-25 11:14:35 -0300

    UI e barreira ser um objeto separado, e mais coisas na UI

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -3,14 +3,16 @@ extends KinematicBody2D
 const VEL_MAXIMA = 500 
 var speed = 300
 var move = Vector2()
+var hp = 10
+var maxhp = 10
 
 func _process(delta): 
-    var rotacao = move.x * 0.03 
-    $col.rotation_degrees = lerp($col.rotation_degrees, rotacao, 0.03)
-    
-    var prev_move = move
-    move.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-    move.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-    
-    move = move.normalized() * speed
-    move = move_and_slide(lerp(prev_move, move, 0.1))
+	var rotacao = move.x * 0.03 
+	$col.rotation_degrees = lerp($col.rotation_degrees, rotacao, 0.03)
+	
+	var prev_move = move
+	move.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	move.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	
+	move = move.normalized() * speed
+	move = move_and_slide(lerp(prev_move, move, 0.1))
